c184f7aaa6e593a9b36dfa3205e9d0a58263e4ee
Thomas Ruoff <ThomasRuoff@gmail.com>
2019-09-06 01:02:33 +0200

    use csv DictReader for dynamic fields

--- toLineProtocol.py
+++ toLineProtocol.py
@@ -15,62 +15,70 @@
 
 # CSV file specs
 #
-# | DB Field    | DB field | CSV Column            | CSV Format
-# |-------------|----------|-----------------------|--------------------------
-# | time        | time     | 1  Time               | 2017/05/19 00:00:11 (UTC)
-# |             |          | 2  durP1              |
-# |             |          | 3  ratioP1            |
-# |             |          | 4  P1                 |
-# |             |          | 5  durP2              |
-# |             |          | 6  ratioP2            |
-# |             |          | 7  P2                 |
-# | SDS_P1      | field    | 8  SDS_P1             | 10.36
-# | SDS_P2      | field    | 9  SDS_P2             | 9.50
-# | temperature | field    | 10 Temp               | 16.00
-# | humidity    | field    | 11 Humidity           | 79.00
-# |             |          | 12 BMP_temperature    |
-# |             |          | 13 BMP_pressure       |
-# |             |          | 14 BME280_temperature |
-# |             |          | 15 BME280_humidity    |
-# |             |          | 16 BME280_pressure    |
-# | samples     | field    | 17 Samples            | 828799
-# | min_micro   | field    | 18 Min_cycle          | 172
-# | max_micro   | field    | 19 Max_cycle          | 25198
-# |             |          | 20 Signal             | -91
-# | node        | tag      | -- --                 | e.g. esp8266-16229960
-#
+# | DB Field           | DB field | CSV Column         | CSV Format
+# |--------------------|----------|--------------------|--------------------------
+# | time               | time     | Time               | 2017/05/19 00:00:11 (UTC)
+# | ?                  | ?        | durP1              |
+# | ?                  | ?        | ratioP1            |
+# | ?                  | ?        | P1                 |
+# | ?                  | ?        | durP2              |
+# | ?                  | ?        | ratioP2            |
+# | ?                  | ?        | P2                 |
+# | SDS_P1             | field    | SDS_P1             | 10.36
+# | SDS_P2             | field    | SDS_P2             | 9.50
+# | PMS_P1             | field    | PMS_P1             |
+# | PMS_P2             | field    | PMS_P2             |
+# | temperature        | field    | Temp               | 16.00
+# | humidity           | field    | Humidity           | 79.00
+# | BMP_temperature    | field    | BMP_temperature    |
+# | BMP_pressure       | field    | BMP_pressure       |
+# | BME280_temperature | field    | BME280_temperature |
+# | BME280_humidity    | field    | BME280_humidity    |
+# | BME280_pressure    | field    | BME280_pressure    |
+# | samples            | field    | Samples            | 828799
+# | min_micro          | field    | Min_cycle          | 172
+# | max_micro          | field    | Max_cycle          | 25198
+# | signal             | field    | Signal             | -91
+# | node               | tag      | N/A filename?      | e.g. esp8266-16229960
+
 
 import os
 import sys
 import csv
 from datetime import datetime, timedelta
 
-def getTimestamp(timestr):
-    naiveDt = datetime.strptime(timestr, '%Y/%m/%d %H:%M:%S');
-    utcTimestamp = (naiveDt - datetime(1970, 1, 1)) / timedelta(seconds=1)
-    return int(utcTimestamp)
+def get_timestamp(timestr):
+    naive_dt = datetime.strptime(timestr, '%Y/%m/%d %H:%M:%S');
+    utc = (naive_dt - datetime(1970, 1, 1)) / timedelta(seconds=1)
+    return int(utc)
 
-sensor_id = os.environ.get('SENSOR_ID', '16229960')
-database = os.environ.get('INFLUXDB_DATABASE', 'feinstaub')
+SENSOR_ID = os.environ.get('SENSOR_ID', '16229960')
+DATABASE = os.environ.get('INFLUXDB_DATABASE', 'sensors')
 
-node = 'esp8266-' + sensor_id
-outline = '{database},node={node} SDS_P1={sds_p1},SDS_P2={sds_p2},humidity={humidity},min_micro={min_micro},max_micro={max_micro},samples={samples},temperature={temperature} {timestamp}'
+NODE = 'esp8266-' + SENSOR_ID
 
-reader = csv.reader(sys.stdin, delimiter=';')
-for row in reader:
-    if (row[0] == 'Time'):
-        continue
+NAME_MAP = {
+    'Humidity': 'humidity',
+    'Max_cycle': 'max_micro',
+    'Samples': 'samples',
+    'Min_cycle': 'min_micro',
+    'Signal': 'signal',
+    'Temp': 'temperature'
+}
 
-    values = {}
-    values['database'] = database
-    values['node'] = node
-    values['timestamp'] = getTimestamp(row[0])
-    values['sds_p1'] = row[7]
-    values['sds_p2'] = row[8]
-    values['temperature'] = row[9]
-    values['humidity'] = row[10]
-    values['samples'] = row[16]
-    values['min_micro'] = row[17]
-    values['max_micro'] = row[18]
+READER = csv.DictReader(sys.stdin, delimiter=';')
+for row in READER:
+    measurements = []
+    for header, value in row.items():
+        if header == 'Time' or not value:
+            continue
+        measurements.append('{0}={1}'.format(NAME_MAP.get(header, header), value))
 
-    print(outline.format(**values))
+    values = {
+        'database': DATABASE,
+        'node': NODE,
+        'measurements': ','.join(measurements),
+        'time': get_timestamp(row['Time'])
+    }
+
+    print('{database},node={node} {measurements} {time}'.format(**values))
